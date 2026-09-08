3a3656b793a8393bbcef3cf4ec54d433c8d9c69b
RobertLib <rob@hotmail.cz>
2026-09-08 19:05:16 +0200

    Initial Commit

---
+++ ToyCars/ToyCarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyCarsApp.swift
+//  ToyCars
+//
+//  Created by Robert Libšanský on 08.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct ToyCarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
